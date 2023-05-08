428241a5799fe97f7cd8cc8dae25ed9d587245a9
pshsh910 <pshsh910@gmail.com>
2023-05-08 22:09:19 +0900

    init: init project

---
+++ src/test/java/com/example/spring_jscode/SpringJsCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_jscode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJsCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
